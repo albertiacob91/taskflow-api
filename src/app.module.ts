29a30447ad27fd370cb8872fbdc35d27eecebf80
Albert Iacob <159893365+alb3rt91@users.noreply.github.com>
2026-02-27 06:43:38 +0100

    feat: add prisma module for database access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, ProjectsModule, TasksModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
