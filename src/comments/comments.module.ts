8593e2c14a3342cb50823656ca768cfaae9d9cf2
Albert Iacob <159893365+alb3rt91@users.noreply.github.com>
2026-03-04 07:51:26 +0100

    File settings: comments, projects, tasks

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ProjectsModule } from '../projects/projects.module';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 
 @Module({
+  imports: [PrismaModule, ProjectsModule], 
   controllers: [CommentsController],
   providers: [CommentsService],
 })
